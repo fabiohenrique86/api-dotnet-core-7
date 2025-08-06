2ceecb1a9b29e78f98dcb6c96bedace37a191d04
fabiohenrique86 <fabio.henrique@robbu.global>
2025-08-06 14:54:14 -0300

    Increasing logging

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -24,6 +24,8 @@ namespace WebApplication2.Controllers
 
             var products = await _iProductService.GetAllAsync();
 
+            _logger.Log(LogLevel.Information, "Product got it");
+
             return Ok(products);
         }
 
@@ -33,6 +35,8 @@ namespace WebApplication2.Controllers
             _logger.Log(LogLevel.Information, $"Getting product {id}");
 
             var product = await _iProductService.GetByIdAsync(id);
+
+            _logger.Log(LogLevel.Information, $"Product {id} got it");
 
             if (product == null) 
                 return NotFound();
@@ -47,6 +51,8 @@ namespace WebApplication2.Controllers
 
             var newProduct = await _iProductService.CreateAsync(product);
 
+            _logger.Log(LogLevel.Information, $"Product {newProduct.Id} posted");
+
             return CreatedAtAction(nameof(Get), new { id = newProduct.Id }, newProduct);
         }
 
@@ -57,8 +63,13 @@ namespace WebApplication2.Controllers
 
             var result = await _iProductService.UpdateAsync(id, product);
 
-            if (!result) 
+            if (!result)
+            {
+                _logger.Log(LogLevel.Information, $"Product {id} was not found");
                 return NotFound();
+            }
+
+            _logger.Log(LogLevel.Information, $"Product put");
 
             return NoContent();
         }
@@ -66,12 +77,17 @@ namespace WebApplication2.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            _logger.Log(LogLevel.Information, $"Deleting product");
+            _logger.Log(LogLevel.Information, $"Deleting product {id}");
 
             var result = await _iProductService.DeleteAsync(id);
 
-            if (!result) 
+            if (!result)
+            {
+                _logger.Log(LogLevel.Information, $"Product {id} was not found");
                 return NotFound();
+            }
+
+            _logger.Log(LogLevel.Information, $"Product deleted");
 
             return NoContent();
         }
